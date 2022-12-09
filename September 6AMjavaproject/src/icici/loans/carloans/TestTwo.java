d47598b03a4c6951928edd031f8e91b01f7f25bc
07saran <ksara73@gmail.com>
2022-12-09 15:08:58 -0500

    added x and y variables in TestTwo

--- September 6AMjavaproject/src/icici/loans/carloans/TestTwo.java
+++ September 6AMjavaproject/src/icici/loans/carloans/TestTwo.java
@@ -1,9 +1,10 @@
 package icici.loans.carloans;
 
-public class TestTwo {
+public class TestTwo
+{   int x=100,y=200;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 
 	}
 
